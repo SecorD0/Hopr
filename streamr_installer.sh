7df966e35ef80ea58d156f7136d7b67a631f0349
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-08 00:09:02 +0300

    Update streamr_installer.sh

--- streamr_installer.sh
+++ streamr_installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt update
+sudo apt install wget -y
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 sudo apt upgrade -y
 sudo apt install wget git build-essential jq expect -y
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/docker_installer.sh)
@@ -22,3 +24,16 @@ expect <<END
 	expect eof
 END
 docker run -it --restart=always --name=streamr_node -d -p 7170:7170 -p 7171:7171 -p 1883:1883 -v $(cd ~/.streamrDocker; pwd):/root/.streamr streamr/broker-node:testnet
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "streamr_log" "docker logs streamr_node --follow --tail=100" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "streamr_wallet_info" ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Streamr/main/wallet_info.sh) | jq" true
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
+echo -e '\nThe node was \e[40m\e[92mstarted\e[0m.\n'
+echo -e 'Remember to save this file:'
+echo -e "\033[0;31m$HOME/.streamrDocker/broker-config.json\e[0m\n"
+echo -e '\tv \e[40m\e[92mUseful commands\e[0m v\n'
+echo -e 'To view the node log: \e[40m\e[92mstreamr_log\e[0m'
+echo -e 'To view the list of all containers: \e[40m\e[92mdocker ps -a\e[0m'
+echo -e 'To delete the node container:'
+echo -e '\e[40m\e[92mdocker stop streamr_node\e[0m'
+echo -e '\e[40m\e[92mdocker container rm streamr_node\e[0m'
+echo -e 'To restart the node: \e[40m\e[92mdocker restart streamr_node\e[0m\n'
