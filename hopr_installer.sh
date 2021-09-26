009be74f3c0a52a49f89de55a296a23aee97dd24
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2021-09-26 13:45:18 +0300

    HOPR node auto-installation

--- hopr_installer.sh
+++ hopr_installer.sh
@@ -5,11 +5,11 @@ sudo apt install wget -y
 read -p $'\e[40m\e[92mEnter the node password:\e[0m ' hopr_password
 sudo apt upgrade -y
 sudo apt install wget git build-essential jq -y
-. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/docker_installer.sh)
+. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/docker_installer.sh) --
 docker pull hopr/hoprd:matic
 docker run -d -it --restart=always -v $HOME/.hoprd-db-matic:/app/db -e DEBUG=hopr* -p 9091:9091 -p 3000:3000 -p 8080:8080 --name=hopr_node hopr/hoprd:matic --password='h0pR-w1Lh0RN' --init --announce --identity /app/db/.hopr-identity --apiToken="h0pR-$hopr_password!" --admin --adminHost 0.0.0.0 --healthCheck --healthCheckHost 0.0.0.0
 docker restart hopr_node
-. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) "hopr_log" "docker logs hopr_node --follow --tail=100" true
+. <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/insert_variable.sh) -n "hopr_log" -v "docker logs hopr_node --follow --tail=100" -a
 . <(wget -qO - https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 echo -e '\nThe node was \e[40m\e[92mstarted\e[0m.\n'
 echo -e 'Remember to save this file:'
